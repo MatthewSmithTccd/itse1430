eb04c020481cec6f2f940dd14feae3694ce9b517
MatthewSmithTccd <matthew.smith146@my.tccd.edu>
2021-02-06 16:42:04 -0600

    Created Budget Project

---
+++ labs/Lab 1/Budget/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Budget
+ * ITSE 1430
+ * Semester 2021 Spring
+ * Matthew Smith
+ */
+
+using System;
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
