fb02d9129c4b959f1a965fa76e4f7c96cf18771b
MatthewSmithTccd <matthew.smith146@my.tccd.edu>
2021-03-31 15:37:13 -0500

    Story 1 and 2 Complete Added Main Menu, and menu items. Added exiting functionality

---
+++ labs/Lab 3/CharacterCreator.WinHost/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace CharacterCreator.WinHost
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.miFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.miCharacterNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.miCharacterEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.miCharacterDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.miHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(282, 28);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miFileExit});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 24);
+            this.toolStripMenuItem1.Text = "&File";
+            // 
+            // miFileExit
+            // 
+            this.miFileExit.Name = "miFileExit";
+            this.miFileExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.miFileExit.Size = new System.Drawing.Size(169, 26);
+            this.miFileExit.Text = "E&xit";
+            this.miFileExit.Click += new System.EventHandler(this.OnFileExit);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miCharacterNew,
+            this.miCharacterEdit,
+            this.toolStripSeparator1,
+            this.miCharacterDelete});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(86, 24);
+            this.toolStripMenuItem2.Text = "&Character";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miHelpAbout});
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(55, 24);
+            this.toolStripMenuItem3.Text = "&Help";
+            // 
+            // miCharacterNew
+            // 
+            this.miCharacterNew.Name = "miCharacterNew";
+            this.miCharacterNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.miCharacterNew.Size = new System.Drawing.Size(175, 26);
+            this.miCharacterNew.Text = "New";
+            // 
+            // miCharacterEdit
+            // 
+            this.miCharacterEdit.Name = "miCharacterEdit";
+            this.miCharacterEdit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.miCharacterEdit.Size = new System.Drawing.Size(175, 26);
+            this.miCharacterEdit.Text = "Edit";
+            // 
+            // miCharacterDelete
+            // 
+            this.miCharacterDelete.Name = "miCharacterDelete";
+            this.miCharacterDelete.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.miCharacterDelete.Size = new System.Drawing.Size(175, 26);
+            this.miCharacterDelete.Text = "Delete";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(172, 6);
+            // 
+            // miHelpAbout
+            // 
+            this.miHelpAbout.Name = "miHelpAbout";
+            this.miHelpAbout.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.miHelpAbout.Size = new System.Drawing.Size(157, 26);
+            this.miHelpAbout.Text = "About";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 403);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.MinimumSize = new System.Drawing.Size(260, 420);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Character Creator";
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem miFileExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem miCharacterNew;
+        private System.Windows.Forms.ToolStripMenuItem miCharacterEdit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem miCharacterDelete;
+        private System.Windows.Forms.ToolStripMenuItem miHelpAbout;
+    }
+}
+
---
+++ labs/Lab 3/CharacterCreator/Character.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Character Creator
+ * ITSE 1430
+ * Spring 2021
+ * Matthew Smith
+ */
+using System;
+
+namespace CharacterCreator
+{
+    public class Character
+    {
+        
+        private string _name = "";
+        private string _profession = "";
+        private string _race = "";
+        private string _biography = "";
+        private int _strengthAttribute;
+        private int _intelligenceAttribute;
+        private int _agilityAttribute;
+        private int _constitutionAttribute;
+        private int _charismaAttribute;
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name
+        {
+            get { return _name ?? ""; }
+            set { _name = value?.Trim() ?? ""; }
+        }
+
+        /// <summary>
+        /// Gets or sets the profession.
+        /// </summary>
+        public string Profession
+        {
+            get { return _profession ?? ""; }
+            set { _profession = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the race.
+        /// </summary>
+        public string Race
+        {
+            get { return _race ?? ""; }
+            set { _race = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the biography.
+        /// </summary>
+        public string Biography
+        {
+            get { return _biography ?? ""; }
+            set { _biography = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the strength attribute.
+        /// </summary>
+        public int StrengthAttribute
+        {
+            get { return _strengthAttribute; }
+            set { _strengthAttribute = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the intelligence attribute.
+        /// </summary>
+        public int IntelligenceAttribute
+        {
+            get { return _intelligenceAttribute; }
+            set { _intelligenceAttribute = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the agility attribute.
+        /// </summary>
+        public int AgilityAttribute
+        {
+            get { return _agilityAttribute; }
+            set { _agilityAttribute = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the constitution attribute.
+        /// </summary>
+        public int ConstitutionAttribute
+        {
+            get { return _constitutionAttribute; }
+            set { _constitutionAttribute = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the charisma attribute.
+        /// </summary>
+        public int CharismaAttribute
+        {
+            get { return _charismaAttribute; }
+            set { _charismaAttribute = value; }
+        }
+    }
+
+    
+
+}
